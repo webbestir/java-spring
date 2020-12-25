ee7c51596cbacbd667b815b5de82104b897d639b
Shahriat Hossain <somel90@gmail.com>
2020-12-25 20:15:40 +0600

    Spring practice

---
+++ spring-demo-one/src/com/mycode/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.mycode.springdemo;
+
+public interface Coach {
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+}
---
+++ spring-demo-one/src/com/mycode/springdemo/TrackCoach.java
@@ -0,0 +1,33 @@
+package com.mycode.springdemo;
+
+public class TrackCoach implements Coach {
+
+	private FortuneService fortuneService;
+
+	public TrackCoach() {
+	}
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just do it: " + this.fortuneService.getFortune();
+	}
+
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+
+	// add a destroy method
+	public void doMyCleanStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanStuff");
+	}
+}
